72f3083b01a321be234293d9229cf7eff59bf698
Shayan Tohidi <shayan.stm48@gmail.com>
2025-06-04 10:53:17 -0400

    Modify ssd.test doc

--- R/SSD.R
+++ R/SSD.R
@@ -1,7 +1,6 @@
 #' Compares random prospects by SSD
 #'
-#'  It compares two random prospects by the second-order stochastic dominance (SSD),
-#'  given the distributions.
+#'  It compares two random prospects by the second-order stochastic dominance (SSD).
 #'
 #' @details
 #' If neither prospect dominates the other, it returns 0.
@@ -10,17 +9,16 @@
 #'  element of the SSD vector must be equal or smaller, and at least one element
 #'  should be smaller for the dominant prospect.
 #'
-#' @seealso [ssd.calc()] for the parameters.
+#' @seealso [ssd.calc()] for the calculation.
 #'
-#' @param dists.obj Distributions object.
-#' @returns A list. Two numeric vectors of corresponding SSD values, and an
-#' integer, indicating of the dominant prospect index.
+#' @param sd.obj StochasticDominance object.
+#' @returns A number. Indicating dominant prospect index.
 #' @examples
-#' dists = createDistributions(outcome1 = c(1,4,7),
-#'                             outcome2 = c(2,3,5),
-#'                             prob1 = c(1/3,1/3,1/3),
-#'                             prob2 = c(1/6,1/6,2/3))
-#'  ssd.test(dists)
+#' sd = createStochasticDominance(outcome1 = c(1,4,7),
+#'                                outcome2 = c(2,3,5),
+#'                                prob1 = c(1/3,1/3,1/3),
+#'                                prob2 = c(1/6,1/6,2/3))
+#' ssd.test(sd)
 #'
 #' @export
 ssd.test = function(sd.obj){
