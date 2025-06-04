3df1421690763248a22319cf246117c1879155c7
Shayan Tohidi <shayan.stm48@gmail.com>
2025-06-04 19:37:49 -0400

    Modify calc.area.below.ssd func

--- R/ASSD_THS.R
+++ R/ASSD_THS.R
@@ -1,26 +1,3 @@
-
-calc.area.below.ssd = function(yield, ssd){
-  area = c()
-  n = length(yield)
-  for (i in 1:(n-1)) {
-    a = calc.area.below.line(yield[i],yield[i+1],ssd[i],ssd[i+1])
-    area = append(area, a)
-  }
-
-  return(area)
-}
-
-calc.area.betw.ssd = function(new.yield.ssd){
-  yield = new.yield.ssd$outcome
-  ssd1 = new.yield.ssd$ssd1
-  ssd2 = new.yield.ssd$ssd2
-
-  area1 = calc.area.below.ssd(yield, ssd1)
-  area2 = calc.area.below.ssd(yield, ssd2)
-
-  return(area1 - area2)
-}
-
 calc.assd.ths = function(area, exp.val){
   total.area = sum(abs(area))
   neg.area = abs(sum(area[area < 0]))
@@ -39,4 +16,26 @@ calc.assd.ths = function(area, exp.val){
 
   return(list(winner = winner, epsilon = epsilon, negative.area = neg.area,
               positive.area = pos.area))
+}
+
+calc.area.betw.ssd = function(new.yield.ssd){
+  yield = new.yield.ssd$outcome
+  ssd1 = new.yield.ssd$ssd1
+  ssd2 = new.yield.ssd$ssd2
+
+  area1 = calc.area.below.ssd(yield, ssd1)
+  area2 = calc.area.below.ssd(yield, ssd2)
+
+  return(area1 - area2)
+}
+
+calc.area.below.ssd = function(outcome, ssd){
+  area = c()
+  n = length(outcome)
+  for (i in 1:(n-1)) {
+    a = calc.area.below.line(outcome[i],outcome[i+1],ssd[i],ssd[i+1])
+    area = append(area, a)
+  }
+
+  return(area)
 }
