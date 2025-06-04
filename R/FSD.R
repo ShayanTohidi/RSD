22c4b252c10b2fa5535790fa65d8357540122ef9
Shayan Tohidi <shayan.stm48@gmail.com>
2025-06-04 10:40:49 -0400

    Modify fsd.test doc

--- R/FSD.R
+++ R/FSD.R
@@ -1,7 +1,6 @@
 #'  Compares random prospects by FSD
 #'
-#'  It compares two random prospects by the first-order stochastic dominance (FSD),
-#'  given the distributions.
+#'  It compares two random prospects by the first-order stochastic dominance (FSD).
 #'
 #'  @details
 #'  If neither prospect dominates the other, it returns 0.
@@ -10,14 +9,14 @@
 #'  element of the CDF vector must be equal or smaller, and at least one element
 #'  should be smaller for the dominant prospect.
 #'
-#'  @param dists.obj Distributions object.
+#'  @param sd.obj StochasticDominance object.
 #'  @returns An integer, indicating the index of the dominant prospect.
 #'  @examples
-#' dists = createDistributions(outcome1 = c(1,4,7),
-#'                             outcome2 = c(2,3,5),
-#'                             prob1 = c(1/3,1/3,1/3),
-#'                             prob2 = c(1/6,1/6,2/3))
-#'  fsd.test(dists)
+#' sd = createStochasticDominance(outcome1 = c(1,4,7),
+#'                                outcome2 = c(2,3,5),
+#'                                prob1 = c(1/3,1/3,1/3),
+#'                                prob2 = c(1/6,1/6,2/3))
+#'  fsd.test(sd)
 #'
 #' @export
 fsd.test = function(sd.obj){
