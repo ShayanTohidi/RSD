7dd9bdd1d38a81a1cc5219c0e83a1cd1c7955b0a
Shayan Tohidi <shayan.stm48@gmail.com>
2025-06-04 10:26:48 -0400

    Delete ssd.calc func

--- R/SSD.R
+++ R/SSD.R
@@ -36,25 +36,4 @@ ssd.test = function(dists.obj){
   return(list(ssd1 = ssd.result$ssd1, ssd2 = ssd.result$ssd2, winner = winner))
 }
 
-#' Calculates the SSD values for a pair of prospects.
-#'
-#' @param dists.obj A Distributions object.
-#' @returns A list, containing two SSD vectors.
-#' @examples
-#' dists = createDistributions(outcome1 = c(1,4,7),
-#'                             outcome2 = c(2,3,5),
-#'                             prob1 = c(1/3,1/3,1/3),
-#'                             prob2 = c(1/6,1/6,2/3))
-#' ssd.calc(dists)
-#'
-ssd.calc = function(dists.obj){
 
-  outcome = dists.obj@outcome
-  cdf1 = dists.obj@cum.prob1
-  cdf2 = dists.obj@cum.prob2
-
-  ssd1 = cumsum(lag(cdf1, default = 0) * (outcome - lag(outcome, default = 0)))
-  ssd2 = cumsum(lag(cdf2, default = 0) * (outcome - lag(outcome, default = 0)))
-
-  return(list(ssd1 = ssd1, ssd2 = ssd2))
-}
