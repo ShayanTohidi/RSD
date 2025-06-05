86730e968e1a3d9863b86ff266ecaa50276337f5
Shayan Tohidi <shayan.stm48@gmail.com>
2025-06-04 21:47:28 -0400

    Modify pos.neg.area.assd.ll func body

--- R/ASSD_LL.R
+++ R/ASSD_LL.R
@@ -75,19 +75,19 @@ pos.neg.area.assd.ll = function(sd.obj){
   i=1
   j=1
   while (i < length(cdf.res$outcome)) {
-    y1 = new.outcome.ssd$outcome.new[j]
+    y1 = new.outcome.ssd$outcome[j]
     cdf1 = sd.obj@cdf1[i]
     cdf2 = sd.obj@cdf2[i]
-    if(sd.obj@outcome[i+1] == new.outcome.ssd$outcome.new[j+1]){
+    if(sd.obj@outcome[i+1] == new.outcome.ssd$outcome[j+1]){
       y2 = sd.obj@outcome[i+1]
-      ssd1 = new.outcome.ssd$ssd1.new[j+1]
-      ssd2 = new.outcome.ssd$ssd2.new[j+1]
+      ssd1 = new.outcome.ssd$ssd1[j+1]
+      ssd2 = new.outcome.ssd$ssd2[j+1]
       i = i + 1
       j = j + 1
     } else {
-      y2 = new.outcome.ssd$outcome.new[j+1]
-      ssd1 = new.outcome.ssd$ssd1.new[j]
-      ssd2 = new.outcome.ssd$ssd2.new[j]
+      y2 = new.outcome.ssd$outcome[j+1]
+      ssd1 = new.outcome.ssd$ssd1[j]
+      ssd2 = new.outcome.ssd$ssd2[j]
       j = j + 1
     }
     area = (y2-y1)*(cdf1-cdf2)
