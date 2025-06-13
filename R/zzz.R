4c678721d8f15c2313e80265d3fe000ef18c07ae
Shayan Tohidi <shayan.stm48@gmail.com>
2025-06-13 12:59:58 -0400

    Modify zzz file, update globalVars

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 utils::globalVariables(c("CDF", "Outcome", "Outcomes", "Prospects", "SSD",
-                         "Yield", "cdf", "cdf1", "cdf2", "prospects", "ssd"
+                         "Yield", "cdf", "cdf1", "cdf2", "prospects", "ssd",
+                         "pr1", "pr2"
                          ))
