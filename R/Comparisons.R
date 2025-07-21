77f983c82d3404cb73bea6f9e7941b3fe0b18726
Shayan Tohidi <shayan.stm48@gmail.com>
2025-07-21 15:51:27 -0400

    create create.dataframe function, to create the base dataframe

--- R/Comparisons.R
+++ R/Comparisons.R
@@ -8,3 +8,20 @@ compare.all = function(variable, probability, outcome, afsd.epsilon = 0.1,
 
   return(data)
 }
+
+create.dataframe = function(variable, probability, outcome){
+
+  if (!(is.character(variable) & is.numeric(probability) & is.numeric(outcome))){
+    stop("Expected types: variable (character), probability and outcome: (numeric)")
+  }
+
+  len = length(variable)
+  if (length(probability) != len | length(outcome) != len) {
+    stop("All input vectors must have the same length.")
+  }
+
+  data = data.frame(variable = variable,
+                    probability = probability,
+                    outcome = outcome)
+  return(data)
+}
