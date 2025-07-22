1bde21778b27388962a701e510f30a6b9fc38695
Shayan Tohidi <shayan.stm48@gmail.com>
2025-07-22 19:47:21 -0400

    Add input validation to compare.all func, removed from create.paired.distributions func

--- R/Comparisons.R
+++ R/Comparisons.R
@@ -2,6 +2,15 @@ compare.all = function(variable, probability, outcome, afsd.epsilon.threshold = 
                        assd.ll.epsilon.threshold = 0.1,
                        assd.ths.epsilon.threshold = 0.1, include.details = TRUE){
 
+  if (!(is.character(variable) & is.numeric(probability) & is.numeric(outcome))){
+    stop("Expected types: variable (character), probability and outcome: (numeric)")
+  }
+
+  len = length(variable)
+  if (length(probability) != len | length(outcome) != len) {
+    stop("All input vectors must have the same length.")
+  }
+
   paired.dists = create.paired.distributions(variable, probability, outcome)
 
   data = sd.asd.test.all(paired.dists, include.details)
