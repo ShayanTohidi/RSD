45e714b5f8571211098aba80af917c78eb162a35
Shayan Tohidi <shayan.stm48@gmail.com>
2025-07-22 19:39:33 -0400

    Modify input parameter names in screen func, put threshold at the end of epsilons

--- R/Comparisons.R
+++ R/Comparisons.R
@@ -1,16 +1,16 @@
-compare.all = function(variable, probability, outcome, afsd.epsilon = 0.1,
-                       assd.ll.epsilon = 0.1, assd.ths.epsilon = 0.1,
-                       include.details = TRUE){
+compare.all = function(variable, probability, outcome, afsd.epsilon.threshold = 0.1,
+                       assd.ll.epsilon.threshold = 0.1,
+                       assd.ths.epsilon.threshold = 0.1, include.details = TRUE){
 
   paired.dists = create.paired.distributions(variable, probability, outcome)
 
   data = sd.test.all(paired.dists, include.details)
 
   fsd = screen(data, 'fsd', 0)
-  afsd = screen(data, 'afsd', afsd.epsilon)
+  afsd = screen(data, 'afsd', afsd.epsilon.threshold)
   ssd = screen(data, 'ssd', 0)
-  assd.ll = screen(data, 'assd.ll', assd.ll.epsilon)
-  assd.ths = screen(data, 'assd.ths', assd.ths.epsilon)
+  assd.ll = screen(data, 'assd.ll', assd.ll.epsilon.threshold)
+  assd.ths = screen(data, 'assd.ths', assd.ths.epsilon.threshold)
 
 
   return(list(data = data, fsd.sets = fsd, afsd.sets = afsd,
