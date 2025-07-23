a90408829b40680cea07aed5c70448edee2c70f6
Shayan Tohidi <shayan.stm48@gmail.com>
2025-07-22 21:06:10 -0400

    Add new global variables

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 utils::globalVariables(c("CDF", "Outcome", "Outcomes", "Prospects", "SSD",
                          "Yield", "cdf", "cdf1", "cdf2", "prospects", "ssd",
-                         "pr1", "pr2"
+                         "pr1", "pr2", "variable2", "data1", "data2", "res",
+                         "avg"
                          ))
