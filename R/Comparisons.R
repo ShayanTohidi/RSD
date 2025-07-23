3a9145536dd93366d8fd6ca59c0784d551609737
Shayan Tohidi <shayan.stm48@gmail.com>
2025-07-22 20:50:26 -0400

    Modify doc for compare.all func

--- R/Comparisons.R
+++ R/Comparisons.R
@@ -1,7 +1,47 @@
+#' Comparing all pairs by all rules, and finding the sets
+#'
+#' It creates a data frame of unique unordered pairs of variables, collects
+#' corresponding distributions, tests all SD and ASD rules on every pairs, and
+#' finds the efficient and inefficient sets for all those rules.
+#'
+#' @details
 #' If the type of each input parameter is incorrect, it will raise an error.
 #'
-#' The length of the input parameters must be equal, otherwise it will raise an
-#' error.
+#' The length of the input parameters `variable`, `probability`, and `outcome`
+#' must be equal, otherwise it will raise an error.
+#'
+#' The output is a list that contains 6 elements: `data` is a data frame including
+#' all unique unordered pairs of variables, corresponding distributions, result
+#' of each rule, and epsilon of ASD rule. The rest 5 elements are lists as well,
+#' corresponding to each rule that includes the efficient and inefficient sets.
+#'
+#' The `data` is a nested data frame, and stores the distribution of each variable
+#' in a single cell. The distribution contains two columns: probability and
+#' outcome.
+#'
+#' @seealso [create.paired.distributions(), sd.asd.test.all(), screen()]
+#'
+#' @param variable A character vector containing the name of all variables.
+#' @param probability A numeric vector containing the probability for each
+#' variable to achieve a particular outcome.
+#' @param outcome A numeric vector containing the outcome values.
+#' @param afsd.epsilon.threshold A number that shows the upper limit for epsilon
+#' of afsd rule. If the epsilon is smaller than or equal to this value, the result
+#' is accepted.
+#' @param assd.ll.epsilon.threshold A number that shows the upper limit for epsilon
+#' of assd.ll rule. If the epsilon is smaller than or equal to this value, the result
+#' is accepted.
+#' @param assd.ths.epsilon.threshold A number that shows the upper limit for epsilon
+#' of assd.ths rule. If the epsilon is smaller than or equal to this value, the result
+#' is accepted.
+#' @param include.details A logical that affects the output data to contain
+#' distributions.
+#' @returns A list of 6 elements, including a data frame and the efficient and
+#' inefficient sets of all rules.
+#' @examples
+#' result = compare.all(data_ex$gen, rep(1/29,377), data_ex$yield)
+#'
+#' @export
 compare.all = function(variable, probability, outcome, afsd.epsilon.threshold = 0.1,
                        assd.ll.epsilon.threshold = 0.1,
                        assd.ths.epsilon.threshold = 0.1, include.details = TRUE){
